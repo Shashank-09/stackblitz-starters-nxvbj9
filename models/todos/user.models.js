241721486810f5142edfd1523cd91ee7e0cb624b
Shashank-09 <114328591+Shashank-09@users.noreply.github.com>
2024-01-28 21:19:11 +0530

    Initial commit

---
+++ models/todos/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      typeof: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      typeof: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      typeof: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
